9e4c024b52067ed7ee103c78b696f0f90135e31f
Kadekk3939 <kadekk3939@gmail.com>
2023-03-16 16:29:30 +0100

    Added basic spring project structure

---
+++ src/main/java/pl/polsl/tab/goji/GojiApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.tab.goji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GojiApplication.class, args);
+	}
+
+}

68cfcaae10510c0ee70025a658bbc0467c4b575b
macirze158 <macirze158@student.polsl.pl>
2023-03-27 12:50:23 +0200

    Added Client entity

---
+++ src/main/java/pl/polsl/tab/goji/model/entity/Client.java
@@ -0,0 +1,29 @@
+package pl.polsl.tab.goji.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "client")
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @NotBlank(message = "Name must not be blank")
+    @Column(name = "Name")
+    private String firstName;
+
+
+    @NotBlank(message = "Email must not be blank")
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}

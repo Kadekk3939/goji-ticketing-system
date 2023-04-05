5f60a6c1fc399255683aecb567cbd89f7d5b87a3
Jolanta Lachman <jolalac346@student.polsl.pl>
2023-04-05 10:53:35 +0200

    Added login component and adjusted the app routing

--- angular-goji/src/app/app.module.ts
+++ angular-goji/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

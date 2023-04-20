59a00066e6115527973c38db1f5ad6ab9d1db636
macirze158 <macirze158@student.polsl.pl>
2023-04-20 21:57:50 +0200

    Added UserWriteModel

---
+++ src/main/java/pl/polsl/tab/goji/model/dto/write/UserWriteModel.java
@@ -0,0 +1,23 @@
+package pl.polsl.tab.goji.model.dto.write;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserWriteModel {
+    @NotBlank(message = "First name must not be blank")
+    private String firstName;
+    @NotBlank(message = "Lsat name must not be blank")
+    private String lastName;
+    @NotBlank(message = "Login must not be blank")
+    private String login;
+    @NotBlank(message = "Password must not be blank")
+    private String password;
+    @NotBlank(message = "Email must not be blank")
+    private String email;
+}

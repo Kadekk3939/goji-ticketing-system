955ef9d342ce2ef7284e39a4727bc36e6f603b01
macirze158 <macirze158@student.polsl.pl>
2023-04-24 10:30:20 +0200

    Added issue write model

---
+++ src/main/java/pl/polsl/tab/goji/model/dto/write/IssueWriteModel.java
@@ -0,0 +1,17 @@
+package pl.polsl.tab.goji.model.dto.write;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IssueWriteModel {
+    @NotBlank(message = "Issue name must not be blank")
+    private String issueName;
+    private String description;
+    private String type;
+}

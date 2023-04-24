e2453b7bea849613a5ac715d16242ca1c0e85b0c
macirze158 <macirze158@student.polsl.pl>
2023-04-24 11:43:17 +0200

    Added client repository

---
+++ src/main/java/pl/polsl/tab/goji/repository/ClientRepository.java
@@ -0,0 +1,16 @@
+package pl.polsl.tab.goji.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.polsl.tab.goji.model.entity.Client;
+
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+    Optional<Client> findClientByClientId(Long clientId);
+
+    Optional<Client> findClientByName(String name);
+
+    Boolean existsClientByNameOrEmailOrPhoneNumber(String name, String email, String phoneNumber);
+
+}

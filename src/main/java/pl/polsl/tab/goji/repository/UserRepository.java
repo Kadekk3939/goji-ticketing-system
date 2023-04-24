2556029b34a0ce297ebe13efb0fdde65b39fd509
macirze158 <macirze158@student.polsl.pl>
2023-04-24 11:18:49 +0200

    Added user repository

---
+++ src/main/java/pl/polsl/tab/goji/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package pl.polsl.tab.goji.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.polsl.tab.goji.model.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUserId(Long userId);
+
+    Optional<User> findUserByLogin(String login);
+
+    Optional<User> findUserByEmail(String email);
+
+    Boolean existsUserByLoginOrEmail(String login, String Email);
+}

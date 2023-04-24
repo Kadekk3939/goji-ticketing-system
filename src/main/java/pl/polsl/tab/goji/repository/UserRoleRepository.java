e8f6ca5fc30354bbf3c679dc362661314de68fb0
macirze158 <macirze158@student.polsl.pl>
2023-04-24 11:57:45 +0200

    Added user role repository

---
+++ src/main/java/pl/polsl/tab/goji/repository/UserRoleRepository.java
@@ -0,0 +1,10 @@
+package pl.polsl.tab.goji.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.polsl.tab.goji.model.entity.UserRole;
+
+import java.util.Optional;
+
+public interface UserRoleRepository extends JpaRepository<UserRole, Long>{
+    Optional<UserRole> findUserRoleByRoleName(String roleName);
+}

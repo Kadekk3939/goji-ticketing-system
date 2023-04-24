194e4b1648c3814f4a28d2f70cc25b08cecd28c6
macirze158 <macirze158@student.polsl.pl>
2023-04-24 10:37:47 +0200

    Added request read model

---
+++ src/main/java/pl/polsl/tab/goji/model/dto/read/RequestReadModel.java
@@ -0,0 +1,21 @@
+package pl.polsl.tab.goji.model.dto.read;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestReadModel {
+    private String requestName;
+    private String description;
+    private String result;
+    private String status;
+    private String type;
+    private LocalDateTime openDate;
+    private LocalDateTime inProgressDate;
+    private LocalDateTime finalizationDate;
+}

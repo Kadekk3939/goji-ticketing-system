bb459c77115ce6658ad91655867258f85089e442
macirze158 <macirze158@student.polsl.pl>
2023-04-24 12:13:16 +0200

    Added task repository

---
+++ src/main/java/pl/polsl/tab/goji/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package pl.polsl.tab.goji.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.polsl.tab.goji.model.entity.Task;
+
+import java.util.Optional;
+
+
+public interface TaskRepository extends JpaRepository<Task, Long>{
+
+    Optional<Task> findTaskByTaskId(Long taskName);
+
+    Optional<Task> findTaskByTaskName(String taskName);
+}

0af1880dcf1fb2758c83e43919544ff39c3b9d32
Kadekk3939 <kadekk3939@gmail.com>
2023-05-09 13:59:48 +0200

    Removed findAll from repository

--- src/main/java/pl/polsl/tab/goji/repository/UserRepository.java
+++ src/main/java/pl/polsl/tab/goji/repository/UserRepository.java
@@ -3,7 +3,6 @@ package pl.polsl.tab.goji.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.polsl.tab.goji.model.entity.User;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,6 +13,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
 
     Boolean existsUserByLoginOrEmail(String login, String Email);
-
-    List<User> findAll();
 }

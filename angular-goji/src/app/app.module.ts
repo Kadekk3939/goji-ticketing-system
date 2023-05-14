bf49383be5d548a4d4590f73a9284867effbcce5
borowybas <bar.borowy@gmail.com>
2023-05-14 19:15:25 +0200

    added dialog window template for adding user

--- angular-goji/src/app/app.module.ts
+++ angular-goji/src/app/app.module.ts
@@ -7,21 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LoginComponent } from './components/login/login.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { UserComponent } from './components/user/user.component';
-import { ListComponent } from './components/list/list.component';
+import {Dialog, ListComponent} from './components/list/list.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatTableModule} from "@angular/material/table";
 import {MatCardModule} from "@angular/material/card";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatInputModule} from "@angular/material/input";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     UserComponent,
-    ListComponent
+    ListComponent,
+    Dialog
   ],
   imports: [
     BrowserModule,
@@ -33,9 +41,20 @@ import {MatCardModule} from "@angular/material/card";
     MatButtonModule,
     MatPaginatorModule,
     MatTableModule,
-    MatCardModule
+    MatCardModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [Dialog],
+  exports: [
+    MatDialogModule
+  ]
 })
 export class AppModule { }
--- angular-goji/src/app/components/list/list.component.ts
+++ angular-goji/src/app/components/list/list.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import { User } from "src/app/interfaces/user";
 import {MatTableDataSource} from "@angular/material/table";
 import {HttpErrorResponse} from "@angular/common/http";
 import {ListService} from "../../services/list.service";
 import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {MatDialogConfig} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-list',
@@ -15,7 +17,8 @@ export class ListComponent implements OnInit {
   public users: User[] | undefined;
   public elements = ['Element1', 'Element2','Element3', 'Element4','Element5', 'Element6'];
   public pageSlice = this.elements.slice(0,2);
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private addDialog: MatDialog) {}
   ngOnInit() {
     //this.getUsers();
 
@@ -44,7 +47,39 @@ export class ListComponent implements OnInit {
     this.pageSlice = this.elements.slice(startIndex,endIndex);
   }
 
+  openAddDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '60%';
+    dialogConfig.height = '70%';
+
+    let dialogRef = this.addDialog.open(Dialog, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
+
+  onClose(){
+
+  }
 }
 
+@Component({
+  selector: 'app-list',
+  templateUrl: 'dialog.html',
+})
+export class Dialog {
+  constructor(
+    public dialogRef: MatDialogRef<Dialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {  }
+
+    onNoClick(){
+      this.dialogRef.close();
+    }
+  hide = true;
+}
 
 

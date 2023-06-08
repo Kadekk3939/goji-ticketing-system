913dbc68a71520f866f0c746345af877f2b34cc0
Maxthon72 <maciekageof@gmail.com>
2023-06-08 17:46:54 +0200

    Starting work on task mapper

---
+++ src/main/java/pl/polsl/tab/goji/mappers/TaskMapper.java
@@ -0,0 +1,16 @@
+package pl.polsl.tab.goji.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import pl.polsl.tab.goji.model.dto.read.TaskReadModel;
+import pl.polsl.tab.goji.model.dto.write.TaskWriteModel;
+import pl.polsl.tab.goji.model.entity.Task;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy. WARN)
+public interface TaskMapper {
+}

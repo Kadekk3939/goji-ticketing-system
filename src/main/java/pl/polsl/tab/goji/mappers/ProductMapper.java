40114ccce92b7932d2cb4839b48935565f1972fc
Maxthon72 <maciekageof@gmail.com>
2023-06-08 21:22:53 +0200

    Starting work on product functionality

--- src/main/java/pl/polsl/tab/goji/mappers/ProductMapper.java
+++ src/main/java/pl/polsl/tab/goji/mappers/ProductMapper.java
@@ -1,4 +1,21 @@
 package pl.polsl.tab.goji.mappers;
 
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+import pl.polsl.tab.goji.model.dto.read.ProductReadModel;
+import pl.polsl.tab.goji.model.dto.write.ProductWriteModel;
+import pl.polsl.tab.goji.model.entity.Product;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface ProductMapper {
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    Product toEntity(ProductWriteModel productWriteModel);
+
+    List<ProductReadModel> map(List<Product> products);
 }
--- src/main/java/pl/polsl/tab/goji/service/ProductService.java
+++ src/main/java/pl/polsl/tab/goji/service/ProductService.java
@@ -1,4 +1,18 @@
 package pl.polsl.tab.goji.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.polsl.tab.goji.mappers.ProductMapper;
+import pl.polsl.tab.goji.repository.ProductRepository;
+
+@Service
 public class ProductService {
+    private final ProductMapper productMapper;
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductMapper productMapper,ProductRepository productRepository){
+        this.productMapper = productMapper;
+        this.productRepository = productRepository;
+    }
 }
